fa0be5b0e95d6ae50668076a15937880a302718f
Aaron Patterson <tenderlove@ruby-lang.org>
2026-03-16 10:59:13 -0700

    simplify benchmark

--- test2.rb
+++ test2.rb
@@ -11,21 +11,23 @@ module A
 end
 
 module B
+  # "Ruby" implementation of strlen
   def self.strlen(x)
-    # bytesize is cached, so not really a "comparison"
-    # It just gives us a high watermark for performance.
-    # This test only shows us the cost of pushing a frame
     x.bytesize
   end
 end
 
 str = "foo"
 
+def ruby(str); B.strlen str; end
+def ffi(str); A.strlen str; end
+def c_ext(str); Strlen.strlen_no_hints str; end
+def jit_hints(str); Strlen.strlen str; end
+
 Benchmark.ips do |x|
-  x.report("strlen-ffi")  { A.strlen(str) } # strlen via FFI
-  x.report("strlen-ruby") { B.strlen(str) } # high watermark
-  x.report("strlen-cext") { Strlen.strlen_no_hints(str) } # C ext
-  x.report("ruby-direct") { str.bytesize }
-  x.report("strlen-zjit") { Strlen.strlen(str) }
+  x.report("strlen-ffi")  { ffi(str)  } # strlen via FFI
+  x.report("strlen-ruby") { ruby(str) } # high watermark
+  x.report("strlen-cext") { c_ext(str) } # C ext
+  x.report("strlen-zjit") { jit_hints(str) }
   x.compare!
 end
